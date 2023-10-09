04a83d2896a1e693c7d36e3758199e09164803bf
Alumno <k.millaldeo01@ufromail.cl>
2023-10-09 19:50:13 -0300

    Escritura del código completo para ambas clases (Calculadora y CarroCompra) con una clase extra llamada Main que ejecuta el programa completo

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -2,44 +2,30 @@ public class Calculadora {
 
 	private int n1;
 	private int n2;
-
+    // Constructor for objects of class Calculadora
 	public Calculadora() {
-		// TODO - implement Calculadora.Calculadora
-		throw new UnsupportedOperationException();
+        // initialise instance variables
+		this.n1 = 0;
+        this.n2 = 0;
 	}
 
-	/**
-	 * 
-	 * @param num1
-	 * @param num2
-	 */
 	public Calculadora(int num1, int num2) {
-		// TODO - implement Calculadora.Calculadora
-		throw new UnsupportedOperationException();
+		this.n1 = num1;
+        this.n2 = num2;
 	}
 
 	public int sumar() {
-		// TODO - implement Calculadora.sumar
-		throw new UnsupportedOperationException();
+		return this.n1+this.n2;
 	}
 
 	public int multiplicar() {
-		// TODO - implement Calculadora.multiplicar
-		throw new UnsupportedOperationException();
+		return this.n1*this.n2;
 	}
 
-	/**
-	 * 
-	 * @param num1
-	 */
 	public void setN1(int num1) {
 		this.n1 = num1;
 	}
 
-	/**
-	 * 
-	 * @param num2
-	 */
 	public void setN2(int num2) {
 		this.n2 = num2;
 	}
