2b1e679d10b78983c9830d36119e53a847e170d6
Alumno <k.millaldeo01@ufromail.cl>
2023-10-09 19:01:05 -0300

    Importación de la clase Calculadora() y CarroCompra() desde el diagrama UML creado en Visual Paradigm

---
+++ src/main/java/CarroCompra.java
@@ -0,0 +1,30 @@
+public class CarroCompra {
+
+	private int[][] productos;
+
+	public CarroCompra() {
+		// TODO - implement CarroCompra.CarroCompra
+		throw new UnsupportedOperationException();
+	}
+
+	public int calcularTotal() {
+		// TODO - implement CarroCompra.calcularTotal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cant
+	 * @param precio
+	 */
+	public int subTotal(int cant, int precio) {
+		// TODO - implement CarroCompra.subTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		// TODO - implement CarroCompra.mostrarTotal
+		throw new UnsupportedOperationException();
+	}
+
+}
